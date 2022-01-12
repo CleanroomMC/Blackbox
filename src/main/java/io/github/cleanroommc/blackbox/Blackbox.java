f463da7451c6229a99adb4f02bc79b9874987119
Rongmario <rongmario@protonmail.com>
2022-01-12 22:51:30 +0000

    Setup

---
+++ src/main/java/io/github/cleanroommc/blackbox/Blackbox.java
@@ -0,0 +1,12 @@
+package io.github.cleanroommc.blackbox;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Blackbox.ID, name = Blackbox.NAME, version = Blackbox.VERSION)
+public class Blackbox {
+
+	public static final String ID = "blackbox";
+	public static final String NAME = "Blackbox Rendering API";
+	public static final String VERSION = "1.0";
+
+}
