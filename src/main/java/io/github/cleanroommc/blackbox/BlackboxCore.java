577431996cca07cac8094ab3a0e6b60755929b6c
Rongmario <rongmario@protonmail.com>
2022-01-14 13:29:30 +0000

    Enabled mixins

---
+++ src/main/java/io/github/cleanroommc/blackbox/BlackboxCore.java
@@ -0,0 +1,46 @@
+package io.github.cleanroommc.blackbox;
+
+import net.minecraftforge.common.ForgeVersion;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+
+@IFMLLoadingPlugin.MCVersion(ForgeVersion.mcVersion)
+@IFMLLoadingPlugin.Name("Blackbox Rendering Core")
+public class BlackboxCore implements IFMLLoadingPlugin {
+
+	public BlackboxCore() {
+		MixinBootstrap.init();
+		Mixins.addConfiguration("mixins.blackbox_resource_injection.json");
+	}
+
+	@Override
+	public String[] getASMTransformerClass() {
+		return new String[0];
+	}
+
+	@Override
+	public String getModContainerClass() {
+		return null;
+	}
+
+	@Nullable
+	@Override
+	public String getSetupClass() {
+		return null;
+	}
+
+	@Override
+	public void injectData(Map<String, Object> data) {
+
+	}
+
+	@Override
+	public String getAccessTransformerClass() {
+		return null;
+	}
+
+}
