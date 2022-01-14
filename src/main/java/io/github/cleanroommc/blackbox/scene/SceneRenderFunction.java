5f4b6f815af3b7a52442093312d4be54e5884caa
Rongmario <rongmario@protonmail.com>
2022-01-14 12:28:18 +0000

    Complete WorldSceneRenderer

--- src/main/java/io/github/cleanroommc/blackbox/scene/SceneRenderFunction.java
+++ src/main/java/io/github/cleanroommc/blackbox/scene/SceneRenderFunction.java
@@ -5,6 +5,6 @@ import net.minecraft.util.BlockRenderLayer;
 @FunctionalInterface
 public interface SceneRenderFunction {
 
-	void apply(boolean isTESR, int pass, BlockRenderLayer layer);
+	void apply(boolean renderingTESR, int pass, BlockRenderLayer layer);
 
 }
