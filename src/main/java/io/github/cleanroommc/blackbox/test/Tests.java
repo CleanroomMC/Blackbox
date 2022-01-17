7a402b1fa4e0714bf9275c3dbcd7f8b7efaa94d6
Rongmario <rongmario@protonmail.com>
2022-01-17 23:51:36 +0000

    More tests

--- src/main/java/io/github/cleanroommc/blackbox/test/Tests.java
+++ src/main/java/io/github/cleanroommc/blackbox/test/Tests.java
@@ -37,16 +37,19 @@ import java.util.Map;
  */
 public class Tests {
 
-	static TestBlock testBlock;
+	static TestBlock layered;
+	static TestBlock notLayered;
 
 	@SubscribeEvent
 	public static void onBlocksRegister(RegistryEvent.Register<Block> event) {
-		event.getRegistry().register(testBlock = new TestBlock());
+		event.getRegistry().register(layered = new TestBlock("layered"));
+		event.getRegistry().register(notLayered = new TestBlock("not_layered"));
 	}
 
 	@SubscribeEvent
 	public static void onItemsRegister(RegistryEvent.Register<Item> event) {
-		event.getRegistry().register(new ItemBlock(testBlock).setRegistryName(testBlock.getRegistryName()));
+		event.getRegistry().register(new ItemBlock(layered).setRegistryName(layered.getRegistryName()));
+		event.getRegistry().register(new ItemBlock(notLayered).setRegistryName(notLayered.getRegistryName()));
 	}
 
 	@SubscribeEvent
@@ -60,12 +63,17 @@ public class Tests {
 				.addSprite("overlay", "blackbox:blocks/diamond_ore_overlay")
 				.addParticleSprite("blocks/stone")
 				.bake();
+		IBakedModel second = new BlackboxModelBakery(ModelTemplate.BLOCK)
+				.addSprite("all", "blocks/stone")
+				.addParticleSprite("blocks/stone")
+				.bake();
 		BlockModelShapes shapes = Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelShapes();
 		try {
 			Field bakedModelStoreField = BlockModelShapes.class.getDeclaredField("bakedModelStore");
 			bakedModelStoreField.setAccessible(true);
 			Map<IBlockState, IBakedModel> bakedModelStore = (Map<IBlockState, IBakedModel>) bakedModelStoreField.get(shapes);
-			bakedModelStore.put(testBlock.getDefaultState(), new DiamondOre(first));
+			bakedModelStore.put(layered.getDefaultState(), new DiamondOre(first));
+			bakedModelStore.put(notLayered.getDefaultState(), new FlatBloom(second));
 		} catch (ReflectiveOperationException e) {
 			e.printStackTrace();
 		}
@@ -73,14 +81,9 @@ public class Tests {
 
 	public static class TestBlock extends Block {
 
-		public TestBlock() {
+		public TestBlock(String name) {
 			super(Material.ROCK);
-			setRegistryName(Blackbox.ID, "test");
-		}
-
-		@Override
-		public BlockRenderLayer getRenderLayer() {
-			return BlockRenderLayer.CUTOUT_MIPPED;
+			setRegistryName(Blackbox.ID, name);
 		}
 
 		@Override
@@ -135,6 +138,70 @@ public class Tests {
 		@Override
 		public boolean isAmbientOcclusion() {
 			return true;
+		}
+
+		@Override
+		public boolean isGui3d() {
+			return true;
+		}
+
+		@Override
+		public boolean isBuiltInRenderer() {
+			return false;
+		}
+
+		@Override
+		public TextureAtlasSprite getParticleTexture() {
+			return model.getParticleTexture();
+		}
+
+		@Override
+		public ItemOverrideList getOverrides() {
+			return model.getOverrides();
+		}
+
+	}
+
+	static class FlatBloom implements IBakedModel {
+
+		final IBakedModel model;
+
+		public FlatBloom(IBakedModel model) {
+			this.model = model;
+		}
+
+		@Override
+		public List<BakedQuad> getQuads(@Nullable IBlockState state, @Nullable EnumFacing side, long rand) {
+			List<BakedQuad> quads = new ArrayList<>();
+			for (BakedQuad quad : model.getQuads(state, side, rand)) {
+				VertexFormat newVertexFormat = new VertexFormat(quad.getFormat()).addElement(DefaultVertexFormats.TEX_2S);
+				UnpackedBakedQuad.Builder builder = new UnpackedBakedQuad.Builder(newVertexFormat);
+				VertexLighterFlat trans = new VertexLighterFlat(Minecraft.getMinecraft().getBlockColors()) {
+					@Override
+					protected void updateLightmap(float[] normal, float[] lightmap, float x, float y, float z) {
+						float lv = (float) (((255 >> 0x04) & 0xF) * 0x20) / 0xFFFF; // Light 255 (max)
+						lightmap[0] = lv;
+						lightmap[1] = lv;
+					}
+					@Override
+					public void setQuadTint(int tint) {
+						// NO OP
+					}
+				};
+				trans.setParent(builder);
+				quad.pipe(trans);
+				builder.setQuadOrientation(quad.getFace());
+				builder.setTexture(quad.getSprite());
+				builder.setApplyDiffuseLighting(false);
+				BakedQuad newQuad = builder.build();
+				quads.add(newQuad);
+			}
+			return quads;
+		}
+
+		@Override
+		public boolean isAmbientOcclusion() {
+			return false;
 		}
 
 		@Override
