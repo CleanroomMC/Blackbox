07fa38f8035fae948defefbce1d40c54fb4e2443
Rongmario <rongmario@protonmail.com>
2022-06-02 18:33:20 +0100

    Slider configs

--- src/main/java/com/cleanroommc/blackbox/config/category/lighting/DynamicDarknessConfig.java
+++ src/main/java/com/cleanroommc/blackbox/config/category/lighting/DynamicDarknessConfig.java
@@ -3,6 +3,7 @@ package com.cleanroommc.blackbox.config.category.lighting;
 import com.cleanroommc.blackbox.Blackbox;
 import net.minecraftforge.common.config.Config;
 import net.minecraftforge.common.config.Config.RangeDouble;
+import net.minecraftforge.common.config.Config.SlidingOption;
 
 @Config(modid = Blackbox.ID, name = "/blackbox/lighting/dynamic_darkness")
 public class DynamicDarknessConfig {
@@ -11,9 +12,11 @@ public class DynamicDarknessConfig {
 
     public static boolean ignoreMoonPhases = false;
 
+    @SlidingOption
     @RangeDouble(min = 0.0F, max = 1.0F)
     public static double minMoonBrightness = 0.1F;
 
+    @SlidingOption
     @RangeDouble(min = 0.0F, max = 1.0F)
     public static double maxMoonBrightness = 0.75F;
 
