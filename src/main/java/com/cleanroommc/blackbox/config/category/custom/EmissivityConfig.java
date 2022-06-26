dca6000dec39a5153ba8d619c0e9848c54868a73
Rongmario <rongmario@protonmail.com>
2022-06-26 15:25:56 +0100

    EmissivityConfig  + RefreshResources config annotation (WIP)

---
+++ src/main/java/com/cleanroommc/blackbox/config/category/custom/EmissivityConfig.java
@@ -0,0 +1,17 @@
+package com.cleanroommc.blackbox.config.category.custom;
+
+import com.cleanroommc.blackbox.Blackbox;
+import com.cleanroommc.blackbox.config.core.RefreshResources;
+import net.minecraftforge.client.resource.VanillaResourceType;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.Config.LangKey;
+
+@Config(modid = Blackbox.ID, name = "/blackbox/custom/emissivity", category = "emissivity")
+@LangKey("blackbox.custom.emissivity")
+public class EmissivityConfig {
+
+    @RefreshResources(VanillaResourceType.TEXTURES)
+    @LangKey("blackbox.custom.emissivity.enabled")
+    public static boolean enabled = true;
+
+}
