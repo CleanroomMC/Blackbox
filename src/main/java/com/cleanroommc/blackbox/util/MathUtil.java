649a08cf0ca01fbd950bb65ef5450a83f8ae698c
Rongmario <rongmario@protonmail.com>
2022-06-26 02:37:27 +0100

    Random helper stuff

--- src/main/java/com/cleanroommc/blackbox/util/MathUtil.java
+++ src/main/java/com/cleanroommc/blackbox/util/MathUtil.java
@@ -1,6 +1,11 @@
 package com.cleanroommc.blackbox.util;
 
+import net.minecraft.util.EnumFacing;
+
 public class MathUtil {
+
+    public static final EnumFacing[] FACINGS = EnumFacing.values(); // Make sure it never mutates
+    public static final int FACINGS_LENGTH = FACINGS.length;
 
     public static float lerp(float delta, float start, float end) {
         return start + delta * (end - start);
