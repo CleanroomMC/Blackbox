17ec86f7fc5df068e09a4d9644ddc31fa000eb68
Rongmario <rongmario@protonmail.com>
2022-06-29 14:33:56 +0100

    Chunk meshing config

---
+++ src/main/java/com/cleanroommc/blackbox/config/category/mesh/ChunkMeshingConfig.java
@@ -0,0 +1,24 @@
+package com.cleanroommc.blackbox.config.category.mesh;
+
+import com.cleanroommc.blackbox.Blackbox;
+import net.minecraft.util.math.MathHelper;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.Config.LangKey;
+import net.minecraftforge.common.config.Config.RangeInt;
+import net.minecraftforge.common.config.Config.SlidingOption;
+
+@Config(modid = Blackbox.ID, name = "/blackbox/mesh/chunk_meshing", category = "chunk_meshing")
+@LangKey("blackbox.mesh.chunk_meshing")
+public class ChunkMeshingConfig {
+
+    @SlidingOption
+    @RangeInt(min = 1, max = 10)
+    @LangKey("blackbox.mesh.chunk_meshing.thread_count")
+    public static int threadCount = MathHelper.clamp(Math.max(Runtime.getRuntime().availableProcessors() / 3, Runtime.getRuntime().availableProcessors() - 6), 1, 10);
+
+    @SlidingOption
+    @RangeInt(min = 1, max = 10)
+    @LangKey("blackbox.mesh.chunk_meshing.thread_priority")
+    public static int threadPriority = Thread.NORM_PRIORITY - 2;
+
+}
