2b0b0e9a67be7c78cd831dfc39673914ca1f548d
Rongmario <rongmario@protonmail.com>
2022-07-25 21:06:09 +0100

    ISetupTerrainNotifier => IRenderGlobalNotifier

--- src/main/java/com/cleanroommc/blackbox/notifiers/IRenderGlobalNotifier.java
+++ src/main/java/com/cleanroommc/blackbox/notifiers/IRenderGlobalNotifier.java
@@ -1,9 +1,16 @@
 package com.cleanroommc.blackbox.notifiers;
 
+import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.client.renderer.culling.ICamera;
 import net.minecraft.entity.Entity;
 
-public interface ISetupTerrainNotifier {
+import javax.annotation.Nullable;
+
+public interface IRenderGlobalNotifier {
+
+    default void refresh(@Nullable WorldClient world) {
+
+    }
 
     default void renderTerrainUpdate(Entity viewEntity, double partialTicks, ICamera camera, int frameCount, boolean playerSpectator) {
 
