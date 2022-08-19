68b3989cf72631ef25afedaa39eeeb629932c2a6
Rongmario <rongmario@protonmail.com>
2022-08-19 02:16:32 +0100

    Remove gui factory as an event listener

--- src/main/java/com/cleanroommc/blackbox/config/BlackboxConfigGuiFactory.java
+++ src/main/java/com/cleanroommc/blackbox/config/BlackboxConfigGuiFactory.java
@@ -2,19 +2,11 @@ package com.cleanroommc.blackbox.config;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.IModGuiFactory;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 
 import java.util.Set;
 
 public class BlackboxConfigGuiFactory implements IModGuiFactory {
-
-    public BlackboxConfigGuiFactory() {
-        if (FMLCommonHandler.instance().getSide().isClient()) {
-            MinecraftForge.EVENT_BUS.register(this);
-        }
-    }
 
     @Override
     public void initialize(Minecraft minecraftInstance) {
